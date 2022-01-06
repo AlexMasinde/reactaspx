bab6ec1a906c62ffddd3a7f90fd27c5373e5f4e0
AlexMasinde <56753745+AlexMasinde@users.noreply.github.com>
2022-01-06 03:08:38 +0300

    updated jsx to js

--- WebApplication10/App_Start/ReactConfig.cs
+++ WebApplication10/App_Start/ReactConfig.cs
@@ -6,13 +6,13 @@ using React;
 
 namespace WebApplication10
 {
-	public static class ReactConfig
-	{
+    public static class ReactConfig
+    {
 		public static void Configure()
 		{
 			JsEngineSwitcher.Current.DefaultEngineName = V8JsEngine.EngineName;
 			JsEngineSwitcher.Current.EngineFactories.AddV8();
-			ReactSiteConfiguration.Configuration.AddScript("~/Scripts/components/App.jsx");
+			ReactSiteConfiguration.Configuration.AddScript("~/Scripts/components/App.js");
 		}
 	}
 }
